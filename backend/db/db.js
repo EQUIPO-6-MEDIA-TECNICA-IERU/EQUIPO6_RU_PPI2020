c242ed7f272532f7b15ceb288eb6cf6581fd563a
Carolina Correa Agudelo <carocorrea2727@gmail.com>
2020-11-17 20:03:30 +0000

    modificacion

---
+++ backend/db/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+// párámetros de conexión
+const mysqlConnection=mysql.createConnection({
+host: 'bmcidgbghbtmaicdkurc-mysql.services.clever-cloud.com',
+user: 'ufvizzgdpph3krif',
+password: '4LYxEpnlf7TpnOV6p8MN',
+database: 'bmcidgbghbtmaicdkurc',
+multipleStatements:true
+});
+//Establecer la conexión a la bd
+mysqlConnection.connect(function(err){
+if(err){
+    console.log(err);
+    return;
+}else{
+    console.log('Base de datos conectado');
+}// fin si
+})// fin connect
+
+// al final de las funciones , callbacks, middleware
+module.exports =mysqlConnection;
